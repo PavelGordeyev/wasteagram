7ea02141f1ab7b5b0958ff0b2b8e1fd767938f76
PavelGordeyev <gordeyev.p@gmail.com>
2021-03-04 21:51:03 -0800

    Refactor list into a widget outside the screen

--- lib/screens/posts_screen.dart
+++ lib/screens/posts_screen.dart
@@ -1,90 +1,19 @@
-import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:intl/intl.dart';
+import 'package:wasteagram/widgets/posts_list.dart';
 
-class PostsScreen extends StatefulWidget {
+class PostsScreen extends StatelessWidget {
   PostsScreen({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _PostsScreenState createState() => _PostsScreenState();
-}
-
-class _PostsScreenState extends State<PostsScreen> {
-
-  // final picker = ImagePicker();
-
-  // File image;
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return ListTile(
-      title: Row(
-        children: [
-          Expanded(
-            child: Text(
-              DateFormat('EEEE, MMMM dd, yyyy').format(DateTime.fromMillisecondsSinceEpoch(document['date_posted'].seconds * 1000)).toString(),
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              document['count'].toString(),
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          )
-        ],
-      ),
-      onTap: () {
-        // print("Should increase votes here");
-      },
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return CircularProgressIndicator();
-          }
-          return Column(
-            children: [
-              Expanded(
-                  child: ListView.builder(
-                  itemExtent: 80.0,
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) =>
-                    _buildListItem(context, snapshot.data.documents[index]),
-                ),
-              ),
-
-            ],
-          );
-        }
-      ),
+      body: PostsList(),
     );
   }
-
-  // Future getImage() async {
-  //   final pickedFile = await picker.getImage(source: ImageSource.gallery);
-  //   image = File(pickedFile.path);
-  //   StorageReference storageReference = FirebaseStorage.instance.ref().child('Example.jpg');
-
-  //   StorageUploadTask uploadTask = storageReference.putFile(image);
-  //   await uploadTask.onComplete;
-  //   final url = await storageReference.getDownloadURL();
-  //   print(url);
-  //   // setState(() {});
-  // }
 }
---
+++ lib/widgets/posts_list.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class PostsList extends StatefulWidget {
+
+  PostsList({Key key}) : super(key: key);
+
+  @override
+  _PostsListState createState() => _PostsListState();
+  
+}
+
+class _PostsListState extends State<PostsList> {
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          return _itemList(snapshot: snapshot);
+        }
+    );
+  }
+
+  Widget _itemList({AsyncSnapshot snapshot}) {
+    return Column(
+      children: [
+        Expanded(
+            child: ListView.builder(
+            itemExtent: 80.0,
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) =>
+              _buildListItem(context, snapshot.data.documents[index]),
+          ),
+        ),
+
+      ],
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(
+            child: Text(
+              DateFormat('EEEE, MMMM dd, yyyy').format(DateTime.fromMillisecondsSinceEpoch(document['date_posted'].seconds * 1000)).toString(),
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              document['count'].toString(),
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          )
+        ],
+      ),
+      onTap: () {
+        // print("Should increase votes here");
+      },
+    );
+  }
+}
