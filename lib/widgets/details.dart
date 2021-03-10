c42a06b00ff4c4a3c2a5a5fb609683abc84ca057
PavelGordeyev <gordeyev.p@gmail.com>
2021-03-09 23:24:18 -0800

    Add image from network url to details.  Add image_url key to Firebase db

--- lib/widgets/details.dart
+++ lib/widgets/details.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:location/location.dart';
 import 'package:wasteagram/widgets/posts_list.dart';
 
 class Details extends StatelessWidget {
@@ -26,13 +28,14 @@ class Details extends StatelessWidget {
               ),
               ],
           ),
-          SizedBox(height: 30.0,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Center(
-                heightFactor: 5.0,
-                child: imagePlaceholder(),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxHeight: 300, maxWidth: 350),
+                  child: Image.network(document['image_url'])
+                ),
               ),
             ],
           ),
@@ -50,6 +53,40 @@ class Details extends StatelessWidget {
   }
 
   Widget imagePlaceholder() {
-    return CircularProgressIndicator();
+    if (document['image_url'] == "")
+      return CircularProgressIndicator();
+    else
+      return Image(image: document['image_url']);
   }
+
+  // void retrieveLocation() async {
+
+  //   LocationData locationData;
+  //   var locationService = Location();
+    
+  //   try {
+  //     var _serviceEnabled = await locationService.serviceEnabled();
+  //     if (!_serviceEnabled) {
+  //       _serviceEnabled = await locationService.requestService();
+  //       if (!_serviceEnabled) {
+  //         print('Failed to enable service. Returning.');
+  //         return;
+  //       }
+  //     }
+
+  //     var _permissionGranted = await locationService.hasPermission();
+  //     if (_permissionGranted == PermissionStatus.denied) {
+  //       _permissionGranted = await locationService.requestPermission();
+  //       if (_permissionGranted != PermissionStatus.granted) {
+  //         print('Location service permission not granted. Returning.');
+  //       }
+  //     }
+
+  //     locationData = await locationService.getLocation();
+  //   } on PlatformException catch (e) {
+  //     print('Error: ${e.toString()}, code: ${e.code}');
+  //     locationData = null;
+  //   }
+  //   locationData = await locationService.getLocation();
+  // }
 }
