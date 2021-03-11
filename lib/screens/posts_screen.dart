b732d4eb7b8662eea6e15f6345d4c7cb9ea331a7
PavelGordeyev <gordeyev.p@gmail.com>
2021-03-10 19:22:43 -0800

    Convert postScreen to stateful widget. Add permissions for images/camera on iOS

--- lib/screens/posts_screen.dart
+++ lib/screens/posts_screen.dart
@@ -2,21 +2,26 @@
 import 'package:flutter/material.dart';
 import 'package:wasteagram/widgets/posts_list.dart';
 
-class PostsScreen extends StatelessWidget {
+class PostsScreen extends StatefulWidget {
   PostsScreen({Key key, this.title}) : super(key: key);
 
-  final String title;
+  @override
+  _PostsScreenState createState() => _PostsScreenState();
 
+  final String title;
+}
+
+class _PostsScreenState extends State<PostsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Column(
         children: [
-          PostsList(title: title,),
+          PostsList(title: widget.title,),
           Container(
             padding: const EdgeInsets.all(50.0),
             child: FloatingActionButton(
