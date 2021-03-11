80856f4caffa4641674a6591870027f9a506019c
PavelGordeyev <gordeyev.p@gmail.com>
2021-03-10 19:43:30 -0800

    Revert post_Screen back to stateless widget

--- lib/screens/posts_screen.dart
+++ lib/screens/posts_screen.dart
@@ -1,27 +1,22 @@
-
 import 'package:flutter/material.dart';
 import 'package:wasteagram/widgets/posts_list.dart';
 
-class PostsScreen extends StatefulWidget {
+class PostsScreen extends StatelessWidget {
   PostsScreen({Key key, this.title}) : super(key: key);
 
-  @override
-  _PostsScreenState createState() => _PostsScreenState();
 
   final String title;
-}
 
-class _PostsScreenState extends State<PostsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Column(
         children: [
-          PostsList(title: widget.title,),
+          PostsList(title: title,),
           Container(
             padding: const EdgeInsets.all(50.0),
             child: FloatingActionButton(
@@ -34,4 +29,5 @@ class _PostsScreenState extends State<PostsScreen> {
       )
     );
   }
+
 }
