6defa7ee5db92a4dd70c19024934cc9baa0ad765
PavelGordeyev <gordeyev.p@gmail.com>
2021-03-11 16:13:36 -0800

    Remove unused import in new post and remove debugging statements

--- lib/widgets/new_post.dart
+++ lib/widgets/new_post.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -137,12 +136,6 @@ class _NewPostState extends State<NewPost> {
       'longitude': post.postLongitude,
       'count': post.postWastedItemCount
     });
-
-    // debugPrint("date: ${post.postDateLong}");
-    // debugPrint("latitude: ${post.postLatitude}");
-    // debugPrint("longitude: ${post.postLongitude}");
-    // debugPrint("image: ${post.postImageURL}");
-    // debugPrint("count: ${post.postWastedItemCount}");
   }
 
   Future _getLocation() async{
