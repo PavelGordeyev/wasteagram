e67ee4b3913f691897d506f4bfb5942db064bbca
PavelGordeyev <gordeyev.p@gmail.com>
2021-03-11 16:47:26 -0800

    Remove unused imports

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wasteagram/models/post.dart';
 import 'package:wasteagram/widgets/details.dart';
